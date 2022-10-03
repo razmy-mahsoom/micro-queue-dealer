fc003bc19a12a0c42ef5fbd9892b87bc288aa70e
razmy-mahsoom <razmy_mahsoom@hotmail.com>
2022-10-03 10:15:12 +0530

    initial commit

---
+++ src/main/java/org/microq/dealer/MicroQueueDealerApplication.java
@@ -0,0 +1,13 @@
+package org.microq.dealer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroQueueDealerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroQueueDealerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/microq/dealer/MicroQueueDealerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.microq.dealer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroQueueDealerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
