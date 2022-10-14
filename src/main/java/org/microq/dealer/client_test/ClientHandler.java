4c42155862df103b708fd492e3a285c164258edf
razmy-mahsoom <razmy_mahsoom@hotmail.com>
2022-10-14 20:20:28 +0530

    Default Interchange and Sequence beans are added

--- src/main/java/org/microq/dealer/client_test/ClientHandler.java
+++ src/main/java/org/microq/dealer/client_test/ClientHandler.java
@@ -1,5 +1,9 @@
 package org.microq.dealer.client_test;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.microq.support.auditor.Chaining;
+
 import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -19,7 +23,7 @@ public class ClientHandler implements Runnable{
             this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             //this.clientUsername = bufferedReader.readLine();
             clientHandlers.add(this);
-            broadcastMessage("Server "+ "new" +" has entered the chat");
+            //broadcastMessage("Server "+ "new" +" has entered the chat");
         }catch (IOException e){
             closeEverything(socket,bufferedReader,bufferedWriter);
         }
@@ -42,22 +46,23 @@ public class ClientHandler implements Runnable{
     }
 
     public void broadcastMessage(String messageToSend){
-//        for (ClientHandler clientHandler:clientHandlers){
-//            try{
-//                if(!clientHandler.clientUsername.equals(clientUsername)){
-//                    clientHandler.bufferedWriter.write(messageToSend);
-//                    clientHandler.bufferedWriter.newLine();
-//                    clientHandler.bufferedWriter.flush();;
-//                }
-//            }catch (IOException e){
-//                closeEverything(socket,bufferedReader,bufferedWriter);
-//            }
-//        }
+        //System.out.println("Message From Builder - "+messageToSend);
+        ObjectMapper mapper = new ObjectMapper();
+        Chaining chaining = null;
+        try {
+            chaining = mapper.readValue(messageToSend, Chaining.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(chaining.getInterchange().getInterchangeName());
+        System.out.println(chaining.getSequence().getSequenceName());
+        System.out.println(chaining.getPath());
+        System.out.println(chaining.getClientType());
     }
 
     public void removerClientHandler(){
         clientHandlers.remove(this);
-        broadcastMessage("Server " + clientUsername + " has left the chat");
+        //broadcastMessage("Server " + clientUsername + " has left the chat");
     }
 
     public void closeEverything(Socket socket, BufferedReader bufferedReader,BufferedWriter bufferedWriter){
